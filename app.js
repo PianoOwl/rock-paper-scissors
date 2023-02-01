3dafc557a5db9d44e98712d083b194f4b62915e7
PianoOwl <seif.kamel@live.com>
2023-02-01 12:16:02 -0400

    Implement 5 round rock, paper, scissors

--- app.js
+++ app.js
@@ -1,51 +1,81 @@
 // Game of rock paper scissors, played in the browser console
 
-// Prompt user for choice, and convert to lower case
-const playerSelection = prompt("Rock, paper, or scissors?").toLowerCase();
-
-// Get computer's choice
-function getComputerChoice() {
-    const possibilities = ["rock", "paper", "scissors"];
-    const computerSelection = possibilities[Math.floor(Math.random() * possibilities.length)];
-    
-    return computerSelection;
-}
-
-computerSelection = getComputerChoice();
+game();
 
 // Determine winner of round
-function playRound(playerSelection, computerSelection) {
+function playRound() {
+    outcome = "";
+
+    computerSelection = getComputerChoice();
+    playerSelection = getPlayerChoice();
+
     if (playerSelection == "rock") {
         if (computerSelection == "rock") {
-            console.log("Tie! Play again");
+            outcome = "Tie! Play again";
         } else if (computerSelection == "paper") {
-            console.log("You lose! Paper beats rock!");
+            outcome = "You lose! Paper beats rock!";
         } else {
-            console.log("You win! Rock beats scissors!");
+            outcome = "You win! Rock beats scissors!";
         }
 
     } else if (playerSelection == "paper") {
         if (computerSelection == "rock") {
-            console.log("You win! Paper beats rock!");
+            outcome = "You win! Paper beats rock!";
         } else if (computerSelection == "paper") {
-            console.log("Tie! Play again");
+            outcome = "Tie! Play again";
         } else {
-            console.log("You lose! Scissors beats paper!");
+            outcome = "You lose! Scissors beats paper!";
         }
 
     } else {
         if (computerSelection == "rock") {
-            console.log("You lose! Rock beats scissors!");
+            outcome = "You lose! Rock beats scissors!";
         } else if (computerSelection == "paper") {
-            console.log("You win! Scissors beats paper!");
+            outcome = "You win! Scissors beats paper!";
         } else {
-            console.log("Tie! Play again");
+            outcome = "Tie! Play again";
         }
     }
-
+    console.log(outcome);
+    return outcome;
 }
 
-console.log(playerSelection);
-console.log(computerSelection);
+// Get computer's choice
+function getComputerChoice() {
+    const possibilities = ["rock", "paper", "scissors"];
+    computerSelection = possibilities[Math.floor(Math.random() * possibilities.length)];
+    
+    return computerSelection;
+}
 
-playRound(playerSelection, computerSelection);
+// Get users choice, and convert to lower case
+function getPlayerChoice() {
+    playerSelection = prompt("Rock, paper, or scissors?").toLowerCase();
+}
+
+function game() {
+    let wins = 0;
+    let losses = 0;
+    let ties = 0;
+    
+    for (let i = 0; i < 5; i++) {
+        playRound();
+        if (outcome.includes("win")) {
+            wins++;
+        } else if (outcome.includes("lose")) {
+            losses++;
+        } else {
+            ties++;
+        }
+    }
+    console.log("Wins: " + wins);
+    console.log("Losses: " + losses);
+    console.log("Ties: " + ties)
+    if (wins > losses) {
+        console.log("Congratulations! You won!")
+    } else if (wins < losses) {
+        console.log("Oh no! You lost!")
+    } else {
+        ("You tied!")
+    }
+}
